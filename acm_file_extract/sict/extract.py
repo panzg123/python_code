981ea7fb5ca6c31f86fab1ead4cda5fac863d37e
PanZhengguang <panzg123@qq.com>
2015-09-12 20:58:29 +0800

    acm_code_merge

--- acm_file_extract/sict/extract.py
+++ acm_file_extract/sict/extract.py
@@ -1,12 +1,28 @@
 # coding=utf-8
 import os.path
-rootdir ="E:\\baiduyun_download\\python\\acm_code"                               # 指明被遍历的文件夹
+import sys, shutil, os, string
+rootdir ="F:\\acm_code"
+destDir = "F:\\acm_code_merge"
 
-for parent,dirnames,filenames in os.walk(rootdir):    #三个参数：分别返回1.父目录 2.所有文件夹名字（不含路径） 3.所有文件名字
-    for dirname in  dirnames:                       #输出文件夹信息
-        print "parent is:" + parent
-        print  "dirname is" + dirname
-    for filename in filenames:
-        print "parent is:" + parent
-        print "filename is:" + filename
-        print "the full name of the file is:" + os.path.join(parent,filename) #输出文件路径信息
+def cpFile(srcPath):
+    fileName = os.path.basename(srcPath)
+    destPath = destDir + os.path.sep + fileName
+    if os.path.exists(srcPath) and not os.path.exists(destPath):
+        print 'cp %s %s' % (srcPath,destPath)
+        shutil.copy(srcPath,destPath)
+def getDir(my_filepath):
+    dir = my_filepath.split('\\')
+    return dir[2]
+
+
+def extract():
+    for parent,dirnames,filenames in os.walk(rootdir):    #三个参数：分别返回1.父目录 2.所有文件夹名字（不含路径） 3.所有文件名字
+        for filename in filenames:
+            print "the full name of the file is:" + os.path.join(parent,filename) #输出文件路径信息
+            if filename.endswith(".c") or filename.endswith(".cpp"):
+                filepath=os.path.join(parent,filename)
+                pre_dir_name = getDir(filepath)
+                cpFile(filepath)
+                os.rename(destDir+"\\"+filename, destDir+"\\"+pre_dir_name+"_"+filename)
+
+extract()
